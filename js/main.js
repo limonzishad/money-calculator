2cc871ebc59eeaa9249edaa7017ee989e15432a0
limonzishad <zishadlimon@gmail.com>
2022-02-17 23:33:31 +0600

    a possible solve

--- js/main.js
+++ js/main.js
@@ -71,16 +71,16 @@ document.getElementById('calculate-button').addEventListener('click', function (
     }
     else {
         clearErrorMassage('wrong-cloth-input');
-    }
-    const updatedTotalExpense = foodExpense + rentExpense + clothExpense;
-    if (updatedTotalExpense > netIncome) {
-        errorMassage2('expense-error-massage');
-    }
-    else {
-        clearErrorMassage('expense-error-massage');
-        updateAmount('new-total-expense', updatedTotalExpense);
-        const updatedBalance = netIncome - updatedTotalExpense;
-        updateAmount('new-balance', updatedBalance);
+        const updatedTotalExpense = foodExpense + rentExpense + clothExpense;
+        if (updatedTotalExpense > netIncome) {
+            errorMassage2('expense-error-massage');
+        }
+        else {
+            clearErrorMassage('expense-error-massage');
+            updateAmount('new-total-expense', updatedTotalExpense);
+            const updatedBalance = netIncome - updatedTotalExpense;
+            updateAmount('new-balance', updatedBalance);
+        }
     }
 });
 // save button
