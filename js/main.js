9a0a37c365d1d8b65890ee6c42cffbc82d80daae
limonzishad <zishadlimon@gmail.com>
2022-02-17 13:29:02 +0600

    calculation completed with issue

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// input field function
+// input field
 function valueFromInputField(inputTextId) {
     const inputText = document.getElementById(inputTextId);
     const inputValue = inputText.value;
@@ -6,24 +6,40 @@ function valueFromInputField(inputTextId) {
     inputText.value = '';
     return changeInputValue;
 }
-// balance calculation function
-/* function updateBalance(totalIncome, totalExpense) {
-    const updatedBalance = totalIncome - totalExpense;
-    return updatedBalance;
-} */
+// balance calculation
+function updateBalance(newBalance) {
+    const updatedBalance = netIncome - updatedTotalExpense;
+    const totalBalance = document.getElementById(newBalance);
+    const previousTotalBalance = totalBalance.innerText;
+    totalBalance.innerText = updatedBalance;
+    return totalBalance;
+}
+// update total expense, balance, saving & remaining balance
+function updateAmount(fieldId, amount) {
+    const totalAmount = document.getElementById(fieldId);
+    const previousTotalBalance = totalAmount.innerText;
+    totalAmount.innerText = amount;
+    return totalAmount;
+}
 // calculate button
-document.getElementById('calculation').addEventListener('click', function () {
+document.getElementById('calculate-button').addEventListener('click', function () {
     const netIncome = valueFromInputField('total-income');
     const foodExpense = valueFromInputField('food-expense');
     const rentExpense = valueFromInputField('rent-expense');
     const clothExpense = valueFromInputField('cloth-expense');
     const updatedTotalExpense = foodExpense + rentExpense + clothExpense;
-    const totalExpense = document.getElementById('new-total-expense');
-    const previousTotalExpense = totalExpense.innerText;
-    totalExpense.innerText = updatedTotalExpense;
+    updateAmount('new-total-expense', updatedTotalExpense);
     const updatedBalance = netIncome - updatedTotalExpense;
-    console.log(updatedBalance);
-    const totalBalance = document.getElementById('new-balance');
-    const previousTotalBalance = totalBalance.innerText;
-    totalBalance.innerText = updatedBalance;
+    updateAmount('new-balance', updatedBalance);
+});
+// save button
+document.getElementById('save-button').addEventListener('click', function () {
+    const saveAmount = valueFromInputField('save-amount');
+    const totalBalanceField = document.getElementById('new-balance');
+    const totalBalanceText = totalBalanceField.innerText;
+    const totalBalance = parseFloat(totalBalanceText);
+    const totalSave = totalBalance * (saveAmount / 100);
+    updateAmount('total-save', totalSave);
+    const balanceRemain = totalBalance - totalSave;
+    updateAmount('balance-remain', balanceRemain);
 });
