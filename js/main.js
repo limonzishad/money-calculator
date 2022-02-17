18ba6e3d39fdba804201e4e8270637b7cc22f17e
limonzishad <zishadlimon@gmail.com>
2022-02-17 21:23:05 +0600

    error check & validation done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// input field
+// value from input field
 function valueFromInputField(inputTextId) {
     const inputText = document.getElementById(inputTextId);
     const inputValue = inputText.value;
@@ -20,16 +20,68 @@ function updateAmount(fieldId, amount) {
     totalAmount.innerText = amount;
     return totalAmount;
 }
+// error massage function
+function errorMassage(wrongInput) {
+    const errorMassage = document.getElementById(wrongInput);
+    const previousErrorMassage = errorMassage.innerText;
+    errorMassage.innerText = 'wrong input';
+    return errorMassage;
+}
+// error massage function 2
+function errorMassage2(errorMassageField) {
+    const errorMassage2 = document.getElementById(errorMassageField);
+    const previousErrorMassage = errorMassage2.innerText;
+    errorMassage2.innerText = "can't expense/save more than income";
+    return errorMassage2;
+}
+//clear error massage
+function clearErrorMassage(clearInput) {
+    const clearMassage = document.getElementById(clearInput);
+    const previousErrorMassage = clearMassage.innerText;
+    clearMassage.innerText = '';
+    return clearMassage;
+}
 // calculate button
 document.getElementById('calculate-button').addEventListener('click', function () {
     const netIncome = valueFromInputField('total-income');
     const foodExpense = valueFromInputField('food-expense');
     const rentExpense = valueFromInputField('rent-expense');
     const clothExpense = valueFromInputField('cloth-expense');
+    // check input
+    if (netIncome < 0) {
+        errorMassage('wrong-income-input');
+    }
+    else {
+        clearErrorMassage('wrong-income-input');
+    }
+    if (foodExpense < 0) {
+        errorMassage('wrong-food-input');
+    }
+    else {
+        clearErrorMassage('wrong-food-input');
+    }
+    if (rentExpense < 0) {
+        errorMassage('wrong-rent-input');
+    }
+    else {
+        clearErrorMassage('wrong-rent-input');
+    }
+    if (clothExpense < 0) {
+        errorMassage('wrong-cloth-input');
+    }
+    else {
+        clearErrorMassage('wrong-cloth-input');
+    }
     const updatedTotalExpense = foodExpense + rentExpense + clothExpense;
-    updateAmount('new-total-expense', updatedTotalExpense);
-    const updatedBalance = netIncome - updatedTotalExpense;
-    updateAmount('new-balance', updatedBalance);
+    if (netIncome >= updatedTotalExpense || updatedTotalExpense == 0) {
+        updateAmount('new-total-expense', updatedTotalExpense);
+        const updatedBalance = netIncome - updatedTotalExpense;
+        updateAmount('new-balance', updatedBalance);
+        clearErrorMassage('expense-error-massage');
+    }
+    else {
+        errorMassage2('expense-error-massage');
+    }
 });
 // save button
 document.getElementById('save-button').addEventListener('click', function () {
@@ -37,8 +89,20 @@ document.getElementById('save-button').addEventListener('click', function () {
     const totalBalanceField = document.getElementById('new-balance');
     const totalBalanceText = totalBalanceField.innerText;
     const totalBalance = parseFloat(totalBalanceText);
-    const totalSave = totalBalance * (saveAmount / 100);
-    updateAmount('total-save', totalSave);
-    const balanceRemain = totalBalance - totalSave;
-    updateAmount('balance-remain', balanceRemain);
+    if (saveAmount < 0) {
+        errorMassage('wrong-saving-input');
+    }
+    else {
+        clearErrorMassage('wrong-saving-input');
+        const totalSave = totalBalance * (saveAmount / 100);
+        if (totalBalance >= totalSave) {
+            const balanceRemain = totalBalance - totalSave;
+            updateAmount('total-save', totalSave);
+            updateAmount('balance-remain', balanceRemain);
+            clearErrorMassage('save-error-massage');
+        }
+        else {
+            errorMassage2('save-error-massage');
+        }
+    }
 });
